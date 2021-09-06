efb42248ced89d10e308674eec6f93c0430ce24f
Chien Yu Kuo <josh820217@gmail.com>
2021-09-06 22:47:50 +0800

    adding reading function

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品, 價格' in line:
+			continue #繼續
+		name, price = line.strip().split(',') #按行處理資料&按行儲存
+		products.append([name, price])
+print(products)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -14,11 +24,13 @@ print(products)
 
 #若print(products[0][0]) = 印出大清單第零格裡面的小清單的第零格儲存格印出來
 
+#印出所有購買紀錄
 for p in products:
 	print(p) #印出清單中的每一個小東西
 	print(p[0], '的價格', p[1]) #印出每一個小清單的第0格 & 第1格
 
+#寫入檔案
 with open('products.csv', 'w', encoding ='utf-8' ) as f: #寫入檔案!!!
-	f.write('Products, Price\n') #adding columns & rectify Grabled
+	f.write('商品, 價格\n') #adding columns & rectify Grabled
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
